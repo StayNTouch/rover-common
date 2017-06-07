0a169fe65f2a6f59823e8c60bbcc0f84b9f10d07
Jonathan Herr <jonathan@stayntouch.com>
2017-06-07 14:15:21 -0400

    CICO-41142 Change params to array

--- pms/lib/snt/pms/rpc/user.rb
+++ pms/lib/snt/pms/rpc/user.rb
@@ -5,7 +5,7 @@ module SNT
         class << self
           # ::SNT::PMS::RPC::User.authenticate_api_user(1)
           def authenticate_api_user(params)
-            api.call('authenticate_api_user', params, namespace: :user, timeout: 30)
+            api.call('authenticate_api_user', [ params ], namespace: :user, timeout: 30)
           end
         end
       end
