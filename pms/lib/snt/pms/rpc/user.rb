5716daae7782eda5601a9bf9afbc7e075f96e3a9
Jonathan Herr <jonathan@stayntouch.com>
2017-06-07 13:43:19 -0400

    CICO-41142 Add rpc class for user

---
+++ pms/lib/snt/pms/rpc/user.rb
@@ -0,0 +1,14 @@
+module SNT
+  module PMS
+    module RPC
+      class User < Base
+        class << self
+          # ::SNT::PMS::RPC::User.authenticate_api_user(1)
+          def authenticate_api_user(params)
+            api.call('authenticate_api_user', params, namespace: :user, timeout: 30)
+          end
+        end
+      end
+    end
+  end
+end
