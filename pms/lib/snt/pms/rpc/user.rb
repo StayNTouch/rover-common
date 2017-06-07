70b318f8509139fafa5fce1dd9c7c63f71bed103
Jonathan Herr <jonathan@stayntouch.com>
2017-06-07 17:49:04 -0400

    CICO-41142 Change parameters to username and password

--- pms/lib/snt/pms/rpc/user.rb
+++ pms/lib/snt/pms/rpc/user.rb
@@ -4,9 +4,9 @@ module SNT
       class User < Base
         class << self
           # Used to authenticate the username and password of a user with an api_user role
-          # ::SNT::PMS::RPC::User.authenticate_api_user(username: 'api_user@stayntouch.com', password: 'admin123')
-          def authenticate_api_user(params)
-            api.call('authenticate_api_user', [ params ], namespace: :user, timeout: 30)
+          # ::SNT::PMS::RPC::User.authenticate_api_user('api_user@stayntouch.com', 'admin123')
+          def authenticate_api_user(username, password)
+            api.call('authenticate_api_user', [ username, password ], namespace: :user, timeout: 30)
           end
         end
       end
