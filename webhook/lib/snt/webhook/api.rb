5e750cbbe6bd77a778cea9a4bc8ba54a441f96f1
Jongmyung Ha <jongmyung@stayntouch.com>
2017-08-11 15:23:44 -0400

    CICO-42893: Fix autoload and requires

--- webhook/lib/snt/webhook/api.rb
+++ webhook/lib/snt/webhook/api.rb
@@ -2,7 +2,7 @@ module SNT
   module Webhook
     module API
       autoload :Base, 'snt/webhook/api/base'
-      autoload :Base, 'snt/webhook/api/webhooks'
+      autoload :Webhooks, 'snt/webhook/api/webhooks'
     end
   end
 end
