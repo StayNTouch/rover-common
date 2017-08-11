0b813f3118e7483e482806f064ec3f2eb206643d
Jongmyung Ha <jongmyung@stayntouch.com>
2017-08-11 11:59:18 -0400

    CICO-42893: Initialize Webhook RPC SDK

---
+++ webhook/lib/snt/webhook/api.rb
@@ -0,0 +1,7 @@
+module SNT
+  module Webhook
+    module API
+      autoload :Base, 'snt/webhook/api/base'
+    end
+  end
+end
---
+++ webhook/lib/snt/webhook/api/webhooks.rb
@@ -0,0 +1,35 @@
+module SNT
+  module Webhook
+    module API
+      class Webhooks < Base
+        class << self
+          # ::SNT::Webhook::API::Webhooks.index(chain_uuid)
+          def index(*args)
+            api.call('index', args, namespace: :webhook, timeout: 60)
+          end
+
+          # ::SNT::Webhook::API::Webhooks.show(uuid)
+          def show(*args)
+            api.call('show', args, namespace: :webhook, timeout: 60)
+          end
+
+          # ::SNT::Webhook::API::Webhooks.create(params)
+          def create(*args)
+            api.call('create', args, namespace: :webhook, timeout: 60)
+          end
+
+          # ::SNT::Webhook::API::Webhooks.update(uuid, params)
+          def update(*args)
+            api.call('update', args, namespace: :webhook, timeout: 60)
+          end
+
+          # ::SNT::Webhook::API::Webhooks.supporting_events
+          def supporting_events
+            api.call('supporting_events', {}, namespace: :webhook, timeout: 60)
+          end
+
+        end
+      end
+    end
+  end
+end
