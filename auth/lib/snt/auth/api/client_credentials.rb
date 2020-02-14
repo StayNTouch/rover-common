c3d2508f558d1722404c9ffbb6b02e1927007338
ganeshsbhat <--replace-all>
2020-02-14 15:12:34 -0500

    CICO-75094 Modification in comment

--- auth/lib/snt/auth/api/client_credentials.rb
+++ auth/lib/snt/auth/api/client_credentials.rb
@@ -3,8 +3,8 @@ module SNT
     module API
       class ClientCredentials < Base
         class << self
-          # Used to validate an OAuth 2.0 access token is present and not expired
-          # ::SNT::Auth::API::AccessToken.validate('Bearer 2105655529552d7ffdbe995c5d9b6e49c614c4e177db73735501773bc71ff431')
+          # Used to generate OAuth credentials based on Application Name and Chain
+          # ::SNT::Auth::API::ClientCredentials.generate(params)
           def generate(params)
             api.call('generate', [params[:chain_uuid], params[:app_name]], namespace: :client_credentials, timeout: 60)
           end
